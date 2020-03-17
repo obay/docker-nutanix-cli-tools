3140d0387ab30c1fdad4ec65bd8ad53b34e16cf7
Obay <ahmad.obay@gmail.com>
2020-03-17 10:48:47 +0300

    Adding run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker run -it --rm --env-file credentials/NutanixCluster obaycloud/docker-nutanix-cli-tools:latest ncli
+docker run -it --rm --env-file credentials/NutanixCluster obaycloud/docker-nutanix-cli-tools:latest ncli
